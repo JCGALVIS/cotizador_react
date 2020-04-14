678c1ebec1f8030f74575384c312285f98f92654
JCGALVIS <jcgalvis6@hotmail.com>
2020-04-14 14:32:57 -0500

    Preparar proyecto.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './componentes/Header'
+
+function App() {
+  return (
+    <Header
+      titulo="Cotizador de seguros"
+    />
+  );
+}
+
+export default App;
