ab5ed47aa39c417b3cc7ce5b07746614c9ddd0ea
JCGALVIS <jcgalvis6@hotmail.com>
2020-04-22 10:30:21 -0500

    Validar formulario.

--- src/componentes/Formulario.js
+++ src/componentes/Formulario.js
@@ -41,6 +41,15 @@ const Boton = styled.button`
   }
 `;
 
+const Error = styled.div`
+  background-color: red;
+  color: white;
+  padding: 1rem;
+  width: 100%;
+  text-align: center;
+  margin-bottom: 2rem;
+`;
+
 const Formulario = () => {
   //Definie useSatet del componenete
 
@@ -49,6 +58,8 @@ const Formulario = () => {
     year: '',
     plan: ''
   });
+
+  const [error, guardarError] = useState(false);
 
   //Extraer los valores de datos
   const { marca, year, plan } = datos;
@@ -61,8 +72,40 @@ const Formulario = () => {
     });
   };
 
+  //Cuando el usuario presiona submit
+  const cotizarSeguro = e =>{
+    e.preventDefault();
+
+    if(marca.trim() === '' || year.trim() === '' || plan.trim() === ''){
+      guardarError(true);
+      return;
+    }
+
+    guardarError(false);
+
+    //Obtener la diferencia de años
+
+    //Por cada año hay que restar el 3%
+
+
+    //Americano 15%
+    //Asiatico 5%
+    //Europeo 30%
+
+
+    //Basico aumenta 20%
+    //Completo 50%
+
+
+    //Total
+
+  };
+
   return (
-    <form>
+    <form
+      onSubmit={cotizarSeguro}
+    >
+      { error ? <Error>Todos los campos son obligatorios.</Error> : null}
       <Campo>
         <Label>Marca</Label>
         <Select
@@ -117,7 +160,7 @@ const Formulario = () => {
           /> Completo
       </Campo>
 
-      <Boton type="button">Cotizar</Boton>
+      <Boton type="submit">Cotizar</Boton>
     </form>
   );
 };
